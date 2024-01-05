36c0efb14645954567c27746766fbd3503f8f492
JosueHernand <joseh1337@gmail.com>
2024-01-05 04:37:05 -0800

    Added the contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+
+      <form>
+        <h2>Name:</h2>
+        <input type="text" />
+        <h2>Email Address:</h2>
+        <input type="text" />
+        <h2>Message:</h2>
+        <input type="text" />
+      </form>
+
+      <button>Submit</button>
+    </div>
+  )
+}
