4190746aa8b64da1a3758bbba32c8f8cc70a6d60
JosueHernand <joseh1337@gmail.com>
2024-01-09 06:31:29 -0800

    Added two projects into portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,22 @@
 import Project from "../components/Project"
+import ReelMeal from "../../assets/images/reelmeal.png"
+import EzBank from "../../assets/images/ezbank.png"
 
 export default function Portfolio() {
   const websites = [
     {
       id: 1,
-      title: 'ex',
-      link: 'ex',
-      archive: 'ex',
-      image: 'ex',
+      title: 'EzBank',
+      link: 'https://ezbank-6a5a197718d2.herokuapp.com/homepage',
+      archive: 'https://github.com/Raymond2811/EzBank',
+      image: 'EzBank',
+    },
+    {
+      id: 2,
+      title: 'ReelMeal',
+      link: 'https://gsandman8.github.io/ReelAndMeal/',
+      archive:'https://github.com/Gsandman8/ReelAndMeal',
+      image: 'ReelMeal',
     },
   ]
 
