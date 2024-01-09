8a95b03665b63bca60a5614b0bc66163b8a7f089
JosueHernand <joseh1337@gmail.com>
2024-01-09 02:37:40 -0800

    Updated the Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,92 @@
+import { useState } from 'react';
+
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setErrorMessage('Name is required');
+      return;
+    }
+    if (!email) {
+      setErrorMessage('Email is required');
+      return;
+    }
+    if (!message) {
+      setErrorMessage('Message is required');
+      return;
+    }
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
     <div>
       <h1>Contact</h1>
+      
+      <form onSubmit={handleFormSubmit}>
 
-      <form>
-        <h2>Name:</h2>
-        <input type="text" />
-        <h2>Email Address:</h2>
-        <input type="text" />
-        <h2>Message:</h2>
-        <input type="text" />
+        <div>
+          <input
+          value={name}
+          name='name'
+          onChange={handleInputChange}
+          type='text'
+          placeholder='Name'
+          required={true}
+          />
+        </div>
+
+        <div>
+          <input
+          value={email}
+          name='email'
+          onChange={handleInputChange}
+          type='email'
+          placeholder='Email'
+          required={true}
+          />
+          </div>
+
+        <div>
+          <textarea
+          value={message}
+          name='message'
+          onChange={handleInputChange}
+          type='text'
+          placeholder='Message'
+          required={true}
+          />
+        </div>
+        <button type='submit'>Submit</button>
       </form>
-
-      <button>Submit</button>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
