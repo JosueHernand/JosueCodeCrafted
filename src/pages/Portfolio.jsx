be802f02ae56a96892cdf72f373af4eda2a81eee
JosueHernand <joseh1337@gmail.com>
2024-01-10 01:01:14 -0800

    Added the remaining four projects into portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,10 @@
 import Project from "../components/Project"
 import ReelMeal from "../../assets/images/reelmeal.png"
 import EzBank from "../../assets/images/ezbank.png"
+import WebFlowText from "../../assets/images/jate.png"
+import Passlock from "../../assets/images/passlock.png"
+import WeatherTracker from "../../assets/images/weatherapp.png"
+import CalendarApp from "../../assets/images/calendarapp.png"
 
 export default function Portfolio() {
   const websites = [
@@ -17,6 +21,34 @@ export default function Portfolio() {
       link: 'https://gsandman8.github.io/ReelAndMeal/',
       archive:'https://github.com/Gsandman8/ReelAndMeal',
       image: ReelMeal,
+    },
+    {
+      id: 3,
+      title: 'WebFlowText',
+      link: 'https://webflowtext.onrender.com/',
+      archive: 'https://github.com/JosueHernand/WebFlowText',
+      image: WebFlowText,
+    },
+    {
+      id: 4,
+      title: 'Passlock',
+      link: 'https://josuehernand.github.io/PassLock/',
+      archive: 'https://github.com/JosueHernand/PassLock',
+      image: Passlock,
+    },
+    {
+      id: 5,
+      title: 'Weather Tracker',
+      link: 'https://josuehernand.github.io/WeatherTracker/',
+      archive: 'https://github.com/JosueHernand/WeatherTracker',
+      image: WeatherTracker,
+    },
+    {
+      id: 6,
+      title: 'Calender App',
+      link: 'https://josuehernand.github.io/CalendarApp/',
+      archive: 'https://github.com/JosueHernand/CalendarApp',
+      image: CalendarApp,
     },
   ]
 
