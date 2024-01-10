21c8dc6edf2a020b73aab55dfe29b0ca16a0e3f6
JosueHernand <joseh1337@gmail.com>
2024-01-09 17:05:50 -0800

    Created the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,4 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
+  return (
+  <a href="https://github.com/JosueHernand"target='_blank'>
+  <FontAwesomeIcon icon={faGithub} size="2x" /></a>
+  );
 }
