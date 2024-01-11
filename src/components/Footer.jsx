a39d37a956da794fa3614f8a3c09abfc80ccb1cf
JosueHernand <joseh1337@gmail.com>
2024-01-11 00:47:51 -0800

    Added color and updated the github icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+const customStyle = {
+  backgroundColor: '#78909C',
+};
+
 export default function Footer() {
   return (
-  <a href="https://github.com/JosueHernand"target='_blank'>
-  <FontAwesomeIcon icon={faGithub} size="2x" /></a>
+    <footer className='fixed-bottom d-flex justify-content-center align-items-center p-3' style={customStyle}>
+      <a href="https://github.com/JosueHernand"target='_blank'>
+        <FontAwesomeIcon icon={faGithub} 
+        size="3x" className='iconStyle githubIcon'/>
+      </a>
+    </footer>
   );
 }
