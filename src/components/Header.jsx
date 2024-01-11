cd3f9a94e066b1f61e64c7b10dd93afe08b3cf66
JosueHernand <joseh1337@gmail.com>
2024-01-10 19:54:32 -0800

    Customized the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import NavTabs from "./NavTabs";
+import "../../src/app.css";
 
 export default function Header() {
   return (
-    <header>
-      <h1>Josue Hernandez</h1>
+    <header className="custom-navbar nav justify-content-between p-4 mb-3">
+      <h1 className="custom-heading">Josue Hernandez</h1>
       <NavTabs />
     </header>
   );
--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -4,7 +4,7 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return(
-    <ul className='nav nav-tabs'>
+    <ul className='nav justify-content-end fs-4'>
 
       <li className='nav-item'>
         <Link
