e9b67219463a3a2e3761a02d477651f2bb7f0d46
JosueHernand <joseh1337@gmail.com>
2024-01-10 23:48:45 -0800

    Updated portfolio with avatar included

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,20 @@
+import avatar from '../assets/images/avatar.png';
+
 export default function About() {
   return(
-    <div>
-      <h1>About Me</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+    <div className='text-color p-3'>
+      <h1 className='mb-4'>About Me</h1>
+      <img src={avatar} alt="Josue Hernandez Avatar" className='mb-4' />
+      <p className="text-dark-grey d-flex justify-content-end mb-4">
+      I'm a dedicated learner and interest in the world of technology. 
+      Growing up, I was always interested in technology and gaming which 
+      drew me towards the world of computer science. Nowadays I'm living 
+      my dream on learning how the enitre world of technology works with 
+      all the coding. As much as I enjoy tech, I also like to venture out 
+      and find see new places and try out new foods from different places. 
+      I'm always willing to try and learn new things since I do like to 
+      explore as well. Moreover, I'm excited to combine my interests and 
+      expertise on this journey.
       </p>
     </div>
   );
