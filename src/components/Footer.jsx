84635704e214ea39d890e322d6e75692b2a8e23e
JosueHernand <joseh1337@gmail.com>
2024-01-12 02:27:27 -0800

    Added two more icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
 const customStyle = {
   backgroundColor: '#946f3a',
@@ -8,9 +8,14 @@ const customStyle = {
 export default function Footer() {
   return (
     <footer className='fixed-bottom d-flex justify-content-center align-items-center p-3' style={customStyle}>
-      <a href="https://github.com/JosueHernand"target='_blank'>
-        <FontAwesomeIcon icon={faGithub} 
-        size="3x" className='iconStyle githubIcon'/>
+      <a href="https://github.com/JosueHernand" target='_blank' className='mx-4'>
+        <FontAwesomeIcon icon={faGithub} size="3x" className='iconStyle' />
+      </a>
+      <a href="https://www.linkedin.com/" target='_blank'>
+        <FontAwesomeIcon icon={faLinkedin} size="3x" className='iconStyle' />
+      </a>
+      <a href="https://stackoverflow.com/users/23234683/josue" target='_blank' className='mx-4'>
+        <FontAwesomeIcon icon={faStackOverflow} size="3x" className='iconStyle' />
       </a>
     </footer>
   );
