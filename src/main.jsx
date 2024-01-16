d50457a4e513063ee54df86e4733b6b3ed973701
JosueHernand <joseh1337@gmail.com>
2024-01-15 21:18:32 -0800

    Lowercased the app.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import './app.css';
 
 import App from './App';
 import About from './pages/About';
